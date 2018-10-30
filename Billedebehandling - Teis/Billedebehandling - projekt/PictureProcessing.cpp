eb1af0538c1e0718684ac3140ef6d6352e9ddc5d
Dumbo Junior <chesb17@student.sdu.dk>
2018-10-30 14:51:09 +0100

    Merged Code
    Something

--- Billedebehandling - Teis/Billedebehandling - projekt/PictureProcessing.cpp
+++ Billedebehandling - Teis/Billedebehandling - projekt/PictureProcessing.cpp
@@ -41,6 +41,56 @@ std::vector<std::vector<std::vector<int>>> PictureProcessing::RGBto8Bit(CImg<uns
 	return pixelValues8Bit;
 }
 
+void PictureProcessing::colorValues2bit(std::vector<std::vector<std::vector<int>>> input)
+{
+	for (int i = 0; i <= 40; i++) {
+		for (int k = 0; k <= 40; k++) {
+			std::bitset<3>	rBit3(input[k][i][0]);
+			std::bitset<3>	gBit3(input[k][i][1]);
+			std::bitset<2>	bBit2(input[k][i][2]);
+
+			std::string stringByte = rBit3.to_string() + gBit3.to_string() + bBit2.to_string();
+
+			std::bitset<8> colorByte(stringByte);
+
+
+			colorValues[k][i] = colorByte;
+
+		}
+	}
+
+}
+
+std::vector<std::vector<std::bitset<8>>> PictureProcessing::getcolourValues()
+{
+	return colorValues;
+}
+
+int PictureProcessing::getR8bitColor()
+{
+	return r8bitColor;
+}
+
+int PictureProcessing::getG8bitColor()
+{
+	return g8bitColor;
+}
+
+int PictureProcessing::getB8bitColor()
+{
+	return b8bitColor;
+}
+
+void PictureProcessing::byteTO8bitcolorvalues(std::bitset<8> byteInput)
+{
+	std::bitset<3> bitR(byteInput.to_string().substr(0, 3));
+	std::bitset<3> bitG(byteInput.to_string().substr(3, 3));
+	std::bitset<2> bitB(byteInput.to_string().substr(7, 2));
+
+	r8bitColor = (int)(bitR.to_ulong());
+	g8bitColor = (int)(bitG.to_ulong());
+	b8bitColor = (int)(bitB.to_ulong());
+}
 
 PictureProcessing::~PictureProcessing()
 {
--- Billedebehandling - Teis/Billedebehandling - projekt/PictureProcessing.h
+++ Billedebehandling - Teis/Billedebehandling - projekt/PictureProcessing.h
@@ -1,5 +1,6 @@
 #include "CImg.h"
 #include <vector>
+#include <bitset>
 
 using namespace cimg_library;
 
@@ -12,6 +13,18 @@ public:
 	CImg<unsigned char> pictureResize(CImg<unsigned char> b);
 	std::vector<std::vector<std::vector<int>>> RGBto8Bit(CImg<unsigned char> b);
 
+	void colorValues2bit(std::vector<std::vector<std::vector<int>>> input);
+	std::vector<std::vector<std::bitset<8>>> getcolourValues();
+	int getR8bitColor();
+	int getG8bitColor();
+	int getB8bitColor();
+	void byteTO8bitcolorvalues(std::bitset<8> byteInput);
+
 	~PictureProcessing();
+protected:
+	std::vector<std::vector<std::bitset<8>>> colorValues;
+	int r8bitColor;
+	int g8bitColor;
+	int b8bitColor;
 };
 
