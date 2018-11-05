ac5b85b4eb02553cd17be6e06ee1dad9c9ec10a9
Dumbo Junior <chesb17@student.sdu.dk>
2018-11-05 16:07:05 +0100

    Best of Goertzler
    fully implemented algorithm. Which outputs the best fit of the DTMF tones. - Isch

--- Billedebehandling - Teis/Billedebehandling - projekt/Behandling.cpp
+++ Billedebehandling - Teis/Billedebehandling - projekt/Behandling.cpp
@@ -6,6 +6,57 @@ Behandling::Behandling()
 {
 }
 
+int Behandling::goertzler(int fs, int f, std::vector<int> sampels)
+{
+	int n = sampels.size();
+	int magnitude = 0;
+	int w0 = 0;
+	int w1 = 0;
+	int w2 = 0;
+	
+	int k = floor((0.5 + (n*f) / fs));
+	float omega = (2 * 3.14159265 / n)*k;
+	float cosine = cos(omega);
+	float sine = sin(omega);
+	float coeff = 2 * cosine;
+
+	for (int i = 0; i < n - 1; i++) {
+		w0 = coeff * w1 - w2 + sampels[i];
+		w2 = w1;
+		w1 = w0;
+	}
+
+	magnitude = sqrt(w1*w1 + w2 * w2 - w1 * w2*coeff);
+	return magnitude;
+}
+
+int Behandling::bestLowDTMF(int fs, std::vector<int> sampels)
+{
+	int a = 1;
+	int lowDTMF[4] = { 697, 770, 852, 941 };
+	
+	for (int i = 0; i < 4; i++) {											// Finder den frekvens med højest værdi blandt de lave DTMF toner
+		if (goertzler(samplingFrekvens, lowDTMF[i], maalinger) > goertzler(samplingFrekvens, a, maalinger)) {
+			a = lowDTMF[i];
+		}
+	}
+
+	return a;
+}
+
+
+int Behandling::bestHighDTMF(int fs, std::vector<int> sampels)
+{
+	int a = 1;
+	int highDTMF[4] = { 1209, 1336,1477,1633 };								
+	for (int i = 0; i < 4; i++) {											// Finder den frekvens med den højeste værdi blandt de høje DTMF toner
+		if (goertzler(samplingFrekvens, highDTMF[i], maalinger) > goertzler(samplingFrekvens, a, maalinger)) {			// måskke kan denne optimeres så gørtzler ikke bliver kaldt to gange
+			a = highDTMF[i];																							// EVT. at den husker den forrige værdi
+
+		}
+	}
+	return a;
+}
 
 Behandling::~Behandling()
 {
--- Billedebehandling - Teis/Billedebehandling - projekt/Behandling.h
+++ Billedebehandling - Teis/Billedebehandling - projekt/Behandling.h
@@ -1,10 +1,17 @@
 #pragma once
 #include <vector>
+#include <math.h>
+#include <string>
 class Behandling
 {
 public:
 	Behandling();
-	int goertzler(int fs, int N, int F, std::vector<int> sampels);
+	int goertzler(int fs, int f, std::vector<int> sampels);
+	int bestLowDTMF(int fs, std::vector<int>sampels);
+	int bestHighDTMF(int fs, std::vector<int>sampels);
 	~Behandling();
+protected:
+	int samplingFrekvens;
+	std::vector<int> maalinger;
 };
 
