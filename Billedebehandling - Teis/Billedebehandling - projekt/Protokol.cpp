e2b1a513c2c4684991d9b81d482a7e82c9001e5c
Teis Aalbæk-Nielsen <teis@aalbaek-nielsen.dk>
2018-11-18 10:41:51 +0100

    Some work on Protokol

--- Billedebehandling - Teis/Billedebehandling - projekt/Protokol.cpp
+++ Billedebehandling - Teis/Billedebehandling - projekt/Protokol.cpp
@@ -9,6 +9,27 @@ Protokol::Protokol(std::string s)
 {
 	startString = s;
 	endString = startString;
+	dataSize = 40;
+}
+
+std::string Protokol::getChecksum()
+{
+	return checksum;
+}
+
+std::string Protokol::getLastBit()
+{
+	return lastBit;
+}
+
+std::string Protokol::getSequenceNumber()
+{
+	return sequenceNumber;
+}
+
+std::string Protokol::getRecievedData()
+{
+	return recievedData;
 }
 
 void Protokol::setToneStart(int s)
@@ -28,4 +49,22 @@ std::string Protokol::getString()
 
 Protokol::~Protokol()
 {
+}
+
+void Protokol::unpacking(std::string s)
+{
+	std::string seqNum = s.substr(0, 3);
+	std::string lastB = s.substr(4);
+	std::string data = s.substr(4, 4 + dataSize);
+	std::string check = s.substr(5 + dataSize, dataSize + 5 + 7);
+
+	sequenceNumber = seqNum;
+	lastBit = lastB;
+	recievedData = data;
+	checksum = check;
+}
+
+void Protokol::setLastBit()
+{
+	lastBit = endString.substr(4)
 }
--- Billedebehandling - Teis/Billedebehandling - projekt/Protokol.h
+++ Billedebehandling - Teis/Billedebehandling - projekt/Protokol.h
@@ -18,13 +18,20 @@ public:
 	std::string trailer();		//Skal kalde og lave et CRC-check på data og header.
 	std::string packing();		//Skal kalde header() og trailer(), så de er samlet i et funktions. De skal alligevel altid kaldes sammen
 	void unpacking(std::string s); //Skal tage en string der repræsenterer en pakke, og dele den op i checksum, lastbit, sekvensnummer og data.
-	void unpackingNAK(std::string s); //Skal
-	std::string setLastBit();	//Skal gå ind i stringen og sætte det rigtige bit højt.
+	void unpackingNAK(std::string s); //Skal stå for at pakke NAK's ud, og returnere de data, som NAK'et indeholder.
+	void setLastBit();	//Skal gå ind i stringen og sætte det rigtige bit højt.
+	
 	bool checkChecksum();		//Skal checke at checksummen er rigtig, og returnere true i så fald.
 	bool checkLastBit();		//Skal checke om lastBit er sat højt, og returnere true hvis den er. Dette er for at vi kan vide hvornår der skal sendes NAKs
 	void sequenceSpot();		//Skal sørge for at data bliver lagt på det rigtige sted i recieveBufferen
-	void makeNAK();				//Skal oprette et NAK i den globale NAK-buffer som indeholder sekvensnummeret på pakken. Skal kun kaldes hvis checkCheckSum() returnerer false
+	void makeNAK(std::string);				//Skal oprette et NAK i den globale NAK-buffer som indeholder sekvensnummeret på pakken. Skal kun kaldes hvis checkCheckSum() returnerer false
 
+	//simple get og set metoder
+	std::string getChecksum();
+	std::string getLastBit();
+	std::string getSequenceNumber();
+	std::string getRecievedData();
+	void setDataSize(int dataS); //skal bruges hvis vi ønsker at ændre datastørrelsen på en protokol. Som default er datastørrelsen sat til 40.
 	void setToneStart(int);
 	void setToneSlut(int);
 	std::string getString();
@@ -36,6 +43,8 @@ protected:
 	std::string endString;
 	int toneNrStart;
 	int toneNrSlut;
+
+	int dataSize;
 
 	//Bruges til når data modtages, skabes af funktionen unpacking()
 	std::string checksum;
